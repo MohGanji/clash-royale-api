18725f6fb81c4dc2a835fec714a92d63ef610c16
MohGanji <mohammad-ganji@outlook.com>
2018-08-24 16:19:06 +0430

    api requests almost complete, loop module added

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
-// const loop_module = require('pheebs_loop_module')
-const fetchData = require('./lib/fetcher')
-const createCards = require('./lib/createCards')
+const doFetch = require('./lib/doFetch')
+const HOUR = 1000 * 60 * 60
 
-async function init() {
-  await createCards()
-  return
-}
-
-async function run() {
-  try {
-    await init()
-    await fetchData()
-  } catch (err) {
-    /* eslint-disable no-console */
-    console.log('ERROR: ', err.message)
-    err.response && console.log('REASON: ', err.response.data.reason)
-    err.response && console.log('MESSAGE: ', err.response.data.message)
-    /* eslint-disable no-console */
-  }
-  return
-}
-
-run()
+setInterval(doFetch, 24 * HOUR)
